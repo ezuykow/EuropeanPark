bd360fa3c374cddc54e3baa6880a11c700240b01
ezuykow <zuy1996@gmail.com>
2023-04-10 11:22:10 +0300

    Initial commit

---
+++ src/main/java/ru/ezuykow/europeanpark/EuropeanParkApplication.java
@@ -0,0 +1,13 @@
+package ru.ezuykow.europeanpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuropeanParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EuropeanParkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ezuykow/europeanpark/EuropeanParkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ezuykow.europeanpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuropeanParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

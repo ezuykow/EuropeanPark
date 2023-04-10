420982779b1f766166b0a6e597704cf0282ec455
ezuykow <zuy1996@gmail.com>
2023-04-10 13:49:39 +0300

    Добавил enum Command в CommandManager

---
+++ src/main/java/ru/ezuykow/europeanpark/commands/CommandManager.java
@@ -0,0 +1,15 @@
+package ru.ezuykow.europeanpark.commands;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ezuykow
+ */
+@Component
+public class CommandManager {
+
+    public enum Command {
+        START
+    }
+
+}
